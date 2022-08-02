82bb0aff2407cb757b542113d80752bb465a858d
Alex Schaffer <aschaffer8770@gmail.com>
2022-08-01 18:38:05 -0700

    functions

--- fundamentals/fundamentals/nested_dictionaries/nested_dictionaries.py
+++ fundamentals/fundamentals/nested_dictionaries/nested_dictionaries.py
@@ -1,19 +1,54 @@
-x = [ [5,2,3], [10,8,9] ] 
-students = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'}
-]
-sports_directory = {
-    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# x = [ [5,2,3], [10,8,9] ] 
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+# z = [ {'x': 10, 'y': 20} ]
+
+# x[1][0] = 15
+# print(x)
+
+# students [0]['last_name'] = "Bryant"
+# print(students)
+
+# sports_directory ['soccer'][0] = "Andres"
+# print(sports_directory)
+
+# z[0]['y'] = 30
+# print(z)
+
+
+# students = [
+#          {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#          {'first_name' : 'John', 'last_name' : 'Rosales'},
+#          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#          {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]
+
+# def iterateDictionary(students):
+#     for each_key in students:
+#         print(each_key)
+
+# iterateDictionary(students)
+
+# def iterateDictionary2(key_name, students):
+#     for each_key in students:
+#         print(students[0])
+
+# iterateDictionary2('first_name', students)
+# iterateDictionary2('last_name', students)
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-z = [ {'x': 10, 'y': 20} ]
+def printInfo(some_dict):
+    for keys in dojo:
+        print(dojo['locations'])
+        print(dojo['instructors'])
 
-x[1][0] = 15
-print(x)
-
-students [0]['last_name'] = "Bryant"
-print(students)
-
-sports_directory ['soccer'][0] = "Andres"
-print(sports_directory)
+printInfo(dojo)
