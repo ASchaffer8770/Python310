a0161895c5b7858cf4226e4c6a999ea1f3eba630
Alex Schaffer <aschaffer8770@gmail.com>
2022-08-02 12:42:58 -0700

    init commit

---
+++ fundamentals/oop/user/user.py
@@ -0,0 +1,36 @@
+class User:
+    def __init__(self,first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        #default attributes
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        print(f"First Name: {self.first_name}")
+        print(f"Last Name: {self.last_name}")
+        print(f"Email: {self.email}")
+        print(f"Age: {self.age}")
+        print(f"Points Balance {self.gold_card_points}")
+        print(f"------------------------------")
+
+    def enroll(self):
+        self.is_rewards_member = True 
+        self.gold_card_points = 200
+        print(f"Welcome to Rewards")
+        print(f"Points balance is {self.gold_card_points}")
+
+    def spend_points(self, amount):
+        self.gold_card_points = self.gold_card_points - amount
+
+first_user = User("John", "Day", "jd@gmail.com", 33)
+
+second_user = User("Cale", "Maker", "ck@gmail.com", 24)
+
+first_user.display_info()
+first_user.enroll()
+first_user.spend_points(152)
+first_user.display_info()
+second_user.display_info()
