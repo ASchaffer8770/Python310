a2550a420813e703c44b45bd29da79e9752ccff1
Alex Schaffer <aschaffer8770@gmail.com>
2022-08-05 14:28:40 -0700

    fumction and route no css

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key="key key"
 
 @app.route("/")
-def counter():
+def home():
     return render_template("index.html")
 
-@app.route("/destroy_session")
+@app.route("/submit", methods=["POST"])
+def count():
+    if "count" not in session:
+        session['count'] = 1
+    else: 
+        session['count'] += 1
+    print(request.form)
+    return redirect('/')
+
+@app.route("/destroy_session", methods=['POST'])
 def destroy():
-    return 
+    session['count'] = 0
+    return redirect('/')
 
 if __name__=="__main__":   
     app.run(debug=True)    
