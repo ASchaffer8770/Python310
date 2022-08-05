a74e34bb63da78d98994559e8129d6614a7fd80e
Alex Schaffer <aschaffer8770@gmail.com>
2022-08-05 11:53:05 -0700

    init commit

---
+++ flask/fundamentals/dojo_fruit_store/server.py
@@ -0,0 +1,27 @@
+from tabnanny import check
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)  
+app.secret_key = "fruits"
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    print(request.form)
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    student_id = request.form['student_id']
+    return render_template('checkout.html')
+
+@app.route('/order_complete')
+def completedOrder():
+    return render_template('complete.html')
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html",)
+
+if __name__=="__main__":   
+    app.run(debug=True)    
