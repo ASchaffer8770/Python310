4e5d6d9c0cd6227eaf77904fcba8c50f814a9f8a
Alex Schaffer <aschaffer8770@gmail.com>
2022-08-11 16:31:40 -0700

    init commit practice

--- flask/fundamentals/hello_flask2/server.py
+++ flask/fundamentals/hello_flask2/server.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def hello_world2():
     return "Hello World2"
 
+@app.route ('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route ('/say/<name>')
+def name(name):
+    return f"Hi " + name
+
+@app.route ('/say/<int:num/<string:word>')
+def multi_name(num, word):
+    output = ''
+    for i in range(0,num):
+        output += f"<p>{word}</p>"
+    return output
+
 if __name__=="__main__": 
     app.run(debug=True)
