ef6577c81c66c439a3a252621c06534163af5e17
Alex Schaffer <aschaffer8770@gmail.com>
2022-08-14 18:23:02 -0700

    add dict and info

--- flask/fundamentals/html_table2/server.py
+++ flask/fundamentals/html_table2/server.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def table():
-    return render_template ("table.html")
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template ("table.html", people = users)
 
 if __name__=="__main__": 
     app.run(debug=True)
